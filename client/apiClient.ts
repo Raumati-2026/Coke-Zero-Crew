02f94f9b5fd58d233afbc7ede43ef9c8fca6913d
Jay Coupe <jay.huston17@gmail.com>
2026-03-13 12:26:28 +1300

    added a models interface

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,5 +1,6 @@
 import request from 'superagent'
 import { WikiFacts } from './models/WikiFacts'
+import { PublicHoliday } from './models/PublicHoliday'
 
 const rootURL = new URL(`/api/v1`, document.baseURI)
 
@@ -37,4 +38,9 @@ export async function getWikiFact() {
 export async function getMeme() {
   const response = await request.get(`${rootURL}/meme`)
   return response.body
+}
+
+export async function getPublicHolidays() {
+  const response = await request.get(`${rootURL}/publicholidays`)
+  return response.body as PublicHoliday
 }
